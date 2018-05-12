d431082aacaf95947544c6e8e88cc3f56c385fca
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2018-05-11 21:51:41 -0500

    Added customization

--- applet.py
+++ applet.py
@@ -2,7 +2,7 @@ import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-x = ([[x,y] for x in range(-10,10) for y in range (-10,10)])
+x = ([[x,y] for x in np.arange(-10,10,0.25) for y in np.arange(-10,10,0.25)])
 
 frames = 300
 
@@ -18,13 +18,15 @@ plt.xlim(-10,10)
 plt.ylim(-10,10)
 
 transform_x, transform_y = transform.T
-scat = ax.scatter(transform_x,transform_y,marker='+')
+scat = ax.scatter(transform_x,transform_y,marker='.',s=0.7)
 
 def update(i,scat):
-    matrix = np.array([[1,1],[0,1]])
-    matrix = (1-sigmoid(i))*np.array([[-2,0],[0,-1]]) + sigmoid(i)*matrix
+    matrix = np.array([[3,1],[3,1]])
+    matrix = (1-sigmoid(i))*np.array([[1,0],[0,1]]) + sigmoid(i)*matrix
     transform = np.array([matrix.dot(k) for k in x])
     scat.set_offsets(transform)
+    if(i % 50 == 0):
+        print("Rendering frame " + str(i) + " out of " + str(frames))
     return scat,
 
 
@@ -32,8 +34,8 @@ def init():
     return scat,
 
 anim = animation.FuncAnimation(fig, update, init_func=init,
-                               frames=300, fargs=[scat], interval=20,repeat=False, blit=True)
+                               frames=frames, fargs=[scat], interval=20,repeat=False, blit=True)
 
-anim.save('the_movie.mp4', fps=50,bitrate=10000,dpi=500)
+anim.save('the_movie.mp4', fps=50,bitrate=5000,dpi=250)
 plt.close('all')
 
