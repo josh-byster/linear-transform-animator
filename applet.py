4b2ed903fab1c9eb4688e7a0467ff70d46c85123
Josh Byster <joshb1050@gmail.com>
2018-05-12 01:53:47 -0500

    Added coordinate system

--- applet.py
+++ applet.py
@@ -2,30 +2,31 @@ import matplotlib.animation as animation
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
 x = ([[x,y] for x in np.arange(-10,10,0.25) for y in np.arange(-10,10,0.25)])
 
-frames = 300
-
+frames = 90
+i_vector = [math.cos(math.pi/3),math.sin(math.pi/3)]
+j_vector = [-math.sin(math.pi/3),math.cos(math.pi/3)]
 def sigmoid(i):
-    return 1/(1+math.exp(-8/frames * i + 4))
+    return min(1.01/(1+math.exp(-12/frames * i + 4)),1)
 
-print(sigmoid(1))
-transform = np.array([matrix.dot(k) for k in x])
 
 fig, ax = plt.subplots()
 
 plt.xlim(-10,10)
 plt.ylim(-10,10)
 
-transform_x, transform_y = transform.T
-scat = ax.scatter(transform_x,transform_y,marker='.',s=0.7)
+scat = ax.scatter([],[],marker='.',s=0.65)
+scat.set_offsets(x)
 
 def update(i,scat):
-    matrix = np.array([[3,1],[3,1]])
+    matrix = np.array([i_vector,j_vector]).T
     matrix = (1-sigmoid(i))*np.array([[1,0],[0,1]]) + sigmoid(i)*matrix
+    ax.quiver([0,0],[0,0],[1,0],[0,1], angles='xy', scale_units='xy', scale=0.2)
     transform = np.array([matrix.dot(k) for k in x])
     scat.set_offsets(transform)
-    if(i % 50 == 0):
+    if(i % 10 == 0):
         print("Rendering frame " + str(i) + " out of " + str(frames))
     return scat,
 
@@ -36,6 +37,6 @@ def init():
 anim = animation.FuncAnimation(fig, update, init_func=init,
                                frames=frames, fargs=[scat], interval=20,repeat=False, blit=True)
 
-anim.save('the_movie.mp4', fps=50,bitrate=5000,dpi=250)
+anim.save('the_movie7.mp4', fps=30,bitrate=5000,dpi=250)
 plt.close('all')
 
