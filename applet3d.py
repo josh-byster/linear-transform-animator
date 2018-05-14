ce3a0abf48e0ca71218e0439b7e4661095384158
Josh Byster <joshb1050@gmail.com>
2018-05-14 03:17:58 -0500

    Cleanup

--- applet3d.py
+++ applet3d.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
-
-# Creates points for a grid from -10 to 10 spaced 0.5 units apart
+# Creates points for a grid from -10 to 10 spaced 1 unit apart
+# The grid is 1,000 points by default (10 x 10 x 10)
 x = ([[x, y, z] for x in np.arange(-10, 10, 2) for y in np.arange(-10, 10, 2) for z in np.arange(-10, 10, 2)])
 
 # Number of frames in the animation
-frames = 30
+frames = 180
 
-# Where the i unit-vector lands
-i_vector = [1, 0, 0]
+# Where the i unit-vector lands (final position)
+i_vector = [0, 1, 0]
 
 # Where the j unit-vector lands
-j_vector = [0, 1, 0]
+j_vector = [-1, 0, 0]
 
 # Where the k unit-vector lands
 k_vector = [0, 0, 1]
@@ -36,14 +36,8 @@ plt.axis('off')
 ax.set_xlim([-10, 10])
 ax.set_ylim([-10, 10])
 ax.set_zlim([-10, 10])
-# plt.zlim(-10, 10)
 scat = ax.scatter([], [], marker='.', s=4)
 scat.set_offsets(x)
-
-
-# Plots the two unit vectors to show what happens to the scaling
-# quiver = ax.quiver([0, 0], [0, 0], [1, 0], [0, 1], color=['g', 'r'], angles='xy', scale_units='xy',
-#                   scale=0.2)
 
 
 def update(i):
@@ -55,9 +49,7 @@ def update(i):
     ax.quiver(0, -10, 0, 0, 20, 0, length=1, color='g', arrow_length_ratio=0.1)
     ax.quiver(-10, 0, 0, 20, 0, 0, length=1, color='g', arrow_length_ratio=0.1)
     # Set vector location - must transpose since we need U and V representing x and y components
-    # of each vector respectively (without transposing, each column represents each unit vector)
-    vector_location = np.array([matrix.dot([1, 0, 0]), matrix.dot([0, 1, 0]), matrix.dot([0, 0, 1])]).T
-    # quiver.set_UVC(vector_location[0], vector_location[1])
+    # of each vector respectively (without transposing, e  ach column represents each unit vector)
     transform = np.array([matrix.dot(k) for k in x])
     scat._offsets3d = [transform[:, 0], transform[:, 1], transform[:, 2]]
     if i % 10 == 0:
@@ -66,4 +58,4 @@ def update(i):
 
 anim = animation.FuncAnimation(fig, update, frames=frames, interval=20, repeat=False, blit=False)
 
-anim.save('example4.mp4', fps=30, bitrate=5000, dpi=800)
+anim.save('rotation2.mp4', fps=30, dpi=1200)
